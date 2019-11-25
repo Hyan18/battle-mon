712cd5d9bbf0a627fa4c7dcf6825fdbe55a2bd93
Hyan18 <Hyan18@github.com>
2019-11-25 16:54:40 +0000

    introducing params

--- app.rb
+++ app.rb
@@ -12,7 +12,12 @@ get '/secret' do
   'new secret'
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  @name = params[:name]
   erb(:index)
 end
